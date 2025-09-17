13ad71718872fc4ede7dc0e48c987714b3d57a32
Charith Lakshan <it23841086@my.sliit.lk>
2025-09-17 18:40:46 +0530

    Update homePage.jsx

--- Frontend/src/pages/homePage.jsx
+++ Frontend/src/pages/homePage.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Header from "../components/header";
 import ProductsPage from "./client/productsPage";
 import ProductOverview from "./client/productOverview";
+import CartPage from "./client/cart";
+import Checkout from "./client/checkout";
 
 export default function HomePage(){
     return(
@@ -13,6 +15,8 @@ export default function HomePage(){
                     <Route path="/" element={<h1>Home page</h1>}/>
                     <Route path="/products" element={<ProductsPage />} />
                     <Route path="/overview/:id" element={<ProductOverview />} />
+                    <Route path="/cart" element={<CartPage />} />
+                    <Route path="/checkout" element={<Checkout />} />
                     <Route path="/contact" element={<h1>Contact page</h1>} />
                     <Route path="/reviews" element={<h1>Reviews page</h1>} />
                     <Route path="/*" element={<h1>404 not found</h1>}/>
