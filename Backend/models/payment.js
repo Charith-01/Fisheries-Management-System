ef1b0b312f24b9355d9ed3e1d0cf25e95147946a
Charith Lakshan <it23841086@my.sliit.lk>
2025-09-24 11:36:03 +0530

    Update payment.js

--- Backend/models/payment.js
+++ Backend/models/payment.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema(
   {
-    orderId: { type: mongoose.Schema.Types.ObjectId, ref: "Order", required: false }, 
+    orderId: { type: mongoose.Schema.Types.ObjectId, ref: "Order", required: false },
+    orderPublicId: { type: String, required: true },
     stripePaymentIntentId: { type: String, required: true },
     amount: { type: Number, required: true },
     currency: { type: String, default: "usd" },
